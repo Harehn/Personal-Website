4a7998a7a78ea1b78f9a669aa97cb3027b41c698
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-10-01 17:21:05 -0400

    Set website to barebones

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="../assets/logo.png"> -->
+    <Home msg="Welcome to my Personal Website"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Home from '@/components/Home.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
