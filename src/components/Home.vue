ebac524b790f8a0c73c53ecbf52728c0e5f8b645
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-10-08 13:21:59 -0400

    Some image css changes

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="home">
-    <h1>{{ msg }}</h1>
-    <div>
-      <p>Welcome to my Website! I'm Harehn Kaundun; my preferred name is Nitin. I earnt my bachelors in Software Engineering from the University of Mcgill, Canada. </p>
-      <p>The two areas I am most experienced in are embedded systems and web development. My favourite language is Python. </p>
-      <p>I am very passionate about programming and always looking forward to learning new things. I like creating new things, so I am open to new projects.</p>
-      <img :src="require(`@/assets/Ring.jpg`)" alt="Picture of me at my Iron Ring Ceremony">
+    <div class="about">
+      <h1>{{ msg }}</h1>
+      <div>
+        <p>Welcome to my Website! I'm Harehn Kaundun; my preferred name is Nitin. I earnt my bachelors in Software Engineering from the University of Mcgill, Canada. </p>
+        <p>The two areas I am most experienced in are embedded systems and web development. My favourite language is Python. </p>
+        <p>I am very passionate about programming and always looking forward to learning new things. I like creating new things, so I am open to new projects.</p>
+        <img class="profile" :src="require(`@/assets/Ring.jpg`)" alt="Picture of me at my Iron Ring Ceremony">
+      </div>
     </div>
-    <div>
+    <div class="gh">
       <img :src="require(`@/assets/Github.jpg`)" alt="Screenshot of My GitHub">
     </div>
 
@@ -40,15 +42,25 @@ a {
   color: #42b983;
 }
 
-img {
+.about {
+  
+}
+
+.profile {
   border: 1px solid #ddd;
   border-radius: 4px;
   padding: 5px;
   width: 150px;
 }
 
-img:hover {
+.profile:hover {
   box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+}
+
+.gh {
+  width: 100%;
+  padding-left: 10%;
+  padding-right: 10%;
 }
 
 </style>
