019fe95b015835e20eb0846c4a764057eb243017
Nitin <harehn.kaundun@mail.mcgill.ca>
2025-10-08 09:40:46 -0400

    Added cards for contact page

---
+++ src/components/Contact.vue
@@ -0,0 +1,79 @@
+<template>
+    
+    <!-- <div>
+      <img :src="require(`@/assets/Github.jpg`)" alt="Screenshot of My GitHub">
+    </div> -->
+
+    <div class="card15">
+    <img src="https://images.pexels.com/photos/1574181/pexels-photo-1574181.jpeg?auto=compress&cs=tinysrgb&w=600" alt="nature-pic-CSSnippets" />
+    <h1>{{ title }}</h1>
+    <p>{{ description }}</p>
+    <a href="#" class="card15-btn">Read More</a>
+    </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Contact',
+  props: {
+    title: String,
+    description: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.card15 {
+  width: 180px;
+  height: 264px;
+  padding: 4px;
+  border-radius: 6px;
+  text-align: center;
+  background-color: #f5f5f5;
+  font-family: "Inter", sans-serif;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  transition: 0.6s ease-in-out;
+}
+
+.card15:hover {
+  box-shadow: 0 18px 35px rgba(50, 50, 90, 0.1), 0 8px 15px rgba(0, 0, 0, 0.07);
+  transform: translateY(-1px);
+}
+
+.card15 img {
+  width: 100%;
+  height: 120px;
+  border-top-left-radius: 5px;
+  border-top-right-radius: 5px;
+}
+
+.card15 h1 {
+  color: #000;
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.card15 p {
+  color: #a0a6ab;
+  font-size: 13px;
+  margin-bottom: 21px;
+}
+
+.card15-btn {
+  cursor: pointer;
+  border-radius: 4px;
+  padding: 10px 20px;
+  color: white;
+  font-size: 14px;
+  background-color: #2e1e8a;
+  text-decoration: none;
+}
+
+.card15-btn:hover {
+  background-color: #4532af;
+}
+
+</style>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,17 +1,33 @@
 <template>
-  <div class="contact">
+  <div class="contactview">
     <h1>This is the contact page</h1>
-    <div>
-    <h3>Email:</h3>
-    <p>Hkaundun@gmail.com</p>
-    </div>
-    <div>
-    <h3>Linked In:</h3>
-    <p>www.linkedin.com/in/harehn-kaundun</p>
-    </div>
-    <div>
-    <h3>GitHub:</h3>
-    <p>https://github.com/Harehn</p>
-    </div>
+    <Contact title="Email" description="Hkaundun@gmail.com"/>
+    <Contact title="Linked In" description="www.linkedin.com/in/harehn-kaundun"/>
+    <Contact title="GitHub" description="https://github.com/Harehn"/>
   </div>
 </template>
+
+<script>
+// @ is an alias to /src
+import Contact from '@/components/Contact.vue'
+
+export default {
+  name: 'ContactView',
+  components: {
+    Contact
+  }
+}
+</script>
+
+<style scoped>
+.contactview{
+  align-content: center;
+  text-align: center;
+}
+.contact{
+  /* display:inline-block; */
+}
+.card15{
+  display: inline-block;
+}
+</style>
